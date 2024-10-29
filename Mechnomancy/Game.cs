6a854e14e193478eaa4061267d2698448a931ea6
moehike <59832236+scarlettjclark@users.noreply.github.com>
2024-10-29 10:29:31 +0000

    initial deck setup implemented

--- Mechnomancy/Game.cs
+++ Mechnomancy/Game.cs
@@ -2,6 +2,20 @@
 {
     public class Game
     {
+        public IList<String> Deck { get; } = [];
 
+        public Game() { 
+            for (int card = 0; card < 10; card++)
+            {
+                if(card < 7)
+                {
+                    Deck.Add("Pyromana");
+                }
+                else
+                {
+                    Deck.Add("Slug");
+                }
+            }
+        }
     }
 }
---
+++ TestProject1/GameTests.cs
@@ -0,0 +1,24 @@
+namespace Mechnomancy.Tests
+{
+    public class GameTests
+    {
+        Game _game;
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void GameInit_DeckStartsOffWithTenCards_DeckCountIsTen()
+        {
+            Assert.That(_game.Deck.Count, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void GameInit_DeckStartsWithSevenPyromana_PyromanaCountInDeckIsSeven()
+        {
+            Assert.That(_game.Deck.Where(card => card.ToString() == "Pyromana").Count(), Is.EqualTo(7));
+        }
+    }
+}
