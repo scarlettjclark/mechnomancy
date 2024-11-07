67a5a4d709368114479ec19df051cb2be524c637
moehike <59832236+scarlettjclark@users.noreply.github.com>
2024-11-07 16:08:05 +0000

    added Rototurret

---
+++ Mechnomancy/Mechnomancy.Tests/RototurretTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechnomancy.Tests
+{
+    public class RototurretTests
+    {
+        [Test]
+        public void Health_RototurretStartsWithCorrectHealth_ReturnsFive()
+        {
+            Rototurret rototurret = new();
+            Assert.That(rototurret.Health, Is.EqualTo(5));
+        }
+    }
+}
---
+++ Mechnomancy/Mechnomancy/Rototurret.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechnomancy
+{
+    public class Rototurret
+    {
+        public int Health { get; set; } = 5;
+    }
+}
