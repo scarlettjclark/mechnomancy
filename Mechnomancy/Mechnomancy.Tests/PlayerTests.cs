858f1cd836fcc43e6bd0a090ff3131ce3526bb09
moehike <59832236+scarlettjclark@users.noreply.github.com>
2024-11-07 14:15:40 +0000

    discard pile implemented

--- Mechnomancy/Mechnomancy.Tests/PlayerTests.cs
+++ Mechnomancy/Mechnomancy.Tests/PlayerTests.cs
@@ -9,11 +9,12 @@
         { 
             _player = new Player(new StartingDeck());
             _deck = _player.Deck;
+            _player.Draw(5);
         }
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(5)]
-        public void Deck_DrawingCardsReducesCardsInDeckByThatAmount_DeckCountDecrements(
+        public void Draw_DrawingCardsReducesCardsInDeckByThatAmount_DeckCountDecrements(
                 int cardsDrawn
             )
         {
@@ -22,20 +23,43 @@
             Assert.That(_deck.Count, Is.EqualTo(initialDeckCount - cardsDrawn));
         }
         [Test]
-        public void Deck_DrawingMoreCardsThanInDeckResultsInException_ThrowsArgumentOutOfRangeException()
+        public void Draw_DrawingMoreCardsThanInDeckAndDiscardResultsInException_ThrowsArgumentOutOfRangeException()
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => _player.Draw(_deck.Count() + 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => _player.Draw(_deck.Count() +_player.DiscardPile.Count + 1));
         }
 
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(5)]
-        public void Draw_DrawAddsThatManyCardsToHand_HandCpuntIncreasesByCardsDrawn(
+        public void Draw_DrawAddsThatManyCardsToHand_HandCountIncreasesByCardsDrawn(
             int cardsDrawn)
         {
             int initialHandCount = _player.Hand.Count();
             _player.Draw(cardsDrawn);
             Assert.That(_player.Hand.Count, Is.EqualTo(initialHandCount + cardsDrawn));
+        }
+        [Test]
+        public void Draw_IfMoreCardsThanInDeckAreDrawnDeckIsReplenished_HandCountIsTen()
+        {
+            _player.Discard();
+            _player.Draw(6);
+            Assert.That(_player.Hand.Count, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Discard_DiscardRemovesCardFromDeck_HandCountDecrements()
+        {
+            int initialHandCount = _player.Hand.Count;
+            _player.Discard();
+            Assert.That(_player.Hand.Count(), Is.EqualTo(initialHandCount - 1));
+        }
+        [Test]
+        public void Discard_DiscardAddsCardToDiscardPile_DiscardPileCountIsOne()
+        {
+            int initialDiscardPileCount = _player.DiscardPile.Count;
+            _player.Discard();
+            Assert.That(_player.DiscardPile.Count, Is.EqualTo(initialDiscardPileCount + 1));
+
         }
 
         [Test]
--- Mechnomancy/Mechnomancy/Player.cs
+++ Mechnomancy/Mechnomancy/Player.cs
@@ -4,21 +4,39 @@
     {
         public IList<Card> Deck { get; private set; }
         public IList<Card> Hand { get; private set; }
+        public IList<Card> DiscardPile { get; private set; }
         public int Pyromana { get; set; } = 0;
 
         public Player(IList<Card> startingDeck)
         {
             Deck = startingDeck;
             Hand = [];
+            DiscardPile = [];
         }
 
         public void Draw(int cardsDrawn)
         {
+            ArgumentOutOfRangeException.ThrowIfLessThan(Deck.Count + DiscardPile.Count, cardsDrawn);
             for (int card = 0; card < cardsDrawn; card++)
             {
+                if (Deck.Count == 0)
+                {
+                    foreach (Card discardedCard in DiscardPile)
+                    {
+                        Deck.Add(discardedCard);
+                        
+                    }
+                    DiscardPile.Clear();
+                }
                 Hand.Add(Deck[0]);
                 Deck.RemoveAt(0);
             }
+        }
+
+        public void Discard()
+        {
+            DiscardPile.Add(Hand[0]);
+            Hand.RemoveAt(0);
         }
     }
 }
